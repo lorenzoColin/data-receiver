687fb73e31189a9d0d9e07d520f771eb14d16fbe
lorenzoColin <lorenzo8399@hotmail.com>
2022-02-22 16:56:58 +0100

    Remove ignored files

---
+++ gmu_online/data-receiver/Controllers/AdminController.cs
@@ -0,0 +1,98 @@
+﻿using data_receiver.Data;
+using data_receiver.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace data_receiver.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: AdminController
+        public ActionResult Index()
+        {
+
+
+            IEnumerable<ApplicationUser> lijst = _db.Users;
+
+            return View(lijst);
+        }
+
+        // GET: AdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
